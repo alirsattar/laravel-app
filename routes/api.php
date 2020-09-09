58c4605109e9338463b7b19ceff64f81e497d3f0
Ali Sattar <alirsattar@gmail.com>
2020-09-09 14:34:43 -0400

    CRUD routes set up

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PlayerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// GET
+Route::get('/players',                  [PlayerController::class, 'index']);
+Route::get('/players/{id}',             [PlayerController::class, 'show']);
+
+// POST
+Route::post('/players',                 [PlayerController::class, 'store']);
+Route::post('/players/{id}/answers',    [PlayerController::class, 'answer']);
+
+// DELETE
+Route::delete('/players/{id}',          [PlayerController::class, 'delete']);
+Route::delete('/players/{id}/answers',  [PlayerController::class, 'resetAnswers']);
